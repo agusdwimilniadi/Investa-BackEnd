4448c5009a760b7b77422e7f5d40cfe111600c09
Agus Dwi <agusdwimill@gmail.com>
2021-11-16 14:25:52 +0700

    littleChanges

--- models/bankCode/bankCode.go
+++ models/bankCode/bankCode.go
@@ -1,10 +1,8 @@
 package bankcode
 
-import "os/user"
-
 type BankCode struct {
-	Id              uint        `gorm:"primaryKey" json:"id"`
-	NamaBank        string      `json:"namaBank"`
-	KodeBank        string      `json:"kodeBank"`
-	ProyekMitraBank []user.User `gorm:"foreignKey:UserDetailsId" json:"proyekMitra" `
+	Id       uint   `gorm:"primaryKey" json:"id"`
+	NamaBank string `json:"namaBank"`
+	KodeBank string `json:"kodeBank"`
+	// ProyekMitraBank  []sektor.
 }
